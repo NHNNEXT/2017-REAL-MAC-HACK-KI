08793587c328a5af47591ad6c8f160735143c6d6
brain <wkddngus5@naver.com>
2017-10-16 23:48:14 +0900

    init_index_file

---
+++ WEB-MAC-HACK-KI/src/main/java/amigo/com/MachackkiApplication.java
@@ -0,0 +1,12 @@
+package amigo.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MachackkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MachackkiApplication.class, args);
+	}
+}
