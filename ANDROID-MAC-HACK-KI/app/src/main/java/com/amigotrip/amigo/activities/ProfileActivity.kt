18f914e42cfbb0a8db6dbae4b4289d515c003d26
지민 <wlals822@naver.com>
2017-10-25 00:48:43 +0900

    config for realase

--- ANDROID-MAC-HACK-KI/app/src/main/java/com/amigotrip/amigo/activities/ProfileActivity.kt
+++ ANDROID-MAC-HACK-KI/app/src/main/java/com/amigotrip/amigo/activities/ProfileActivity.kt
@@ -1,13 +1,13 @@
-package com.zimincom.amigo.activities
+package com.amigotrip.amigo.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.zimincom.amigo.R
+import com.amigotrip.amigo.R
 
-class SearchActivity : AppCompatActivity() {
+class ProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search)
+        setContentView(R.layout.activity_profile)
     }
 }
--- ANDROID-MAC-HACK-KI/app/src/main/java/com/amigotrip/amigo/adpaters/IntroPagerAdapter.kt
+++ ANDROID-MAC-HACK-KI/app/src/main/java/com/amigotrip/amigo/adpaters/IntroPagerAdapter.kt
@@ -1,10 +1,10 @@
-package com.zimincom.amigo.adpaters
+package com.amigotrip.amigo.adpaters
 
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentStatePagerAdapter
-import com.zimincom.amigo.fragments.IntroPage1Fragment
-import com.zimincom.amigo.fragments.IntroPage2Fragment
+import com.amigotrip.amigo.fragments.IntroPage1Fragment
+import com.amigotrip.amigo.fragments.IntroPage2Fragment
 
 /**
  * Created by Zimincom on 2017. 10. 22..
