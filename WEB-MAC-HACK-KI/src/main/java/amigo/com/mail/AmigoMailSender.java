4de9c5a7f771d3d87ebc6cb0e29570c070b987b8
brain <wkddngus5@naver.com>
2017-10-24 22:33:04 +0900

    mail sender

---
+++ WEB-MAC-HACK-KI/src/main/java/amigo/com/mail/AmigoMailSender.java
@@ -0,0 +1,98 @@
+package amigo.com.mail;
+
+import amigo.com.domain.PartyGuest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+/**
+ * Created by Naver on 2017. 10. 24..
+ */
+
+@Slf4j
+public class AmigoMailSender {
+    String emailFromAddress = "amigotrip82@gmail.com";
+
+    public void postMail(String recipients[], String subject, String message, String from) throws MessagingException {
+        boolean debug = false;
+        java.security.Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
+
+        String SMTP_HOST_NAME = "gmail-smtp.l.google.com";
+
+        // Properties 설정
+        Properties props = new Properties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.starttls.enable","true");
+        props.put("mail.smtp.host", SMTP_HOST_NAME);
+        props.put("mail.smtp.auth", "true");
+
+        Authenticator auth = new SMTPAuthenticator();
+        Session session = Session.getDefaultInstance(props, auth);
+
+        session.setDebug(debug);
+
+        // create a message
+        Message msg = new MimeMessage(session);
+
+        // set the from and to address
+        InternetAddress addressFrom = new InternetAddress(from);
+        msg.setFrom(addressFrom);
+
+        InternetAddress[] addressTo = new InternetAddress[recipients.length];
+        for (int i = 0; i < recipients.length; i++) {
+            addressTo[i] = new InternetAddress(recipients[i]);
+        }
+        msg.setRecipients(Message.RecipientType.TO, addressTo);
+
+        // Setting the Subject and Content Type
+        msg.setSubject(subject);
+        msg.setContent(message, "text/html");
+        Transport.send(msg);
+    }
+
+    private String sendMail(String[] emailList, String emailMsgTxt, String emailSubjectTxt) {
+        try {
+            // 메일보내기
+            postMail(emailList, emailSubjectTxt, emailMsgTxt, emailFromAddress);
+            log.debug("mail sended to {}", emailList);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+        return "Success";
+    }
+
+    public String sendPartyGuestFormMail(PartyGuest partyGuest) {
+        String[] toSend = {partyGuest.getEmail()};
+
+        String emailSubject = "Hi " + partyGuest.getName() + ", this is Amigo!";
+        String emailTxt = "<h4>Nice to meet you <strong>" + partyGuest.getName() + "</strong>.<br>" +
+                "your request was successfully verified.<br>" +
+                "Amigos on the trip will check the contents and if there is a suitable friend,<br>" +
+                "They will contact you by email. I hope you have a good trip. thank you.<br><br></h4>" +
+
+                "<h3>You applied with the following content:</h3><br>" +
+                "<h5>Age: " + partyGuest.getAge() + "<br>" +
+                "Gender: " + partyGuest.getGender() + "<br>" +
+                "Language: " + partyGuest.getLanguage() + "<br>" +
+                "Desired date: " + partyGuest.getDate() + "<br>" +
+                "Theme: " + partyGuest.getTheme() + "<br>" +
+                "Attarction: " + partyGuest.getAttraction() + "</h5>";
+        sendMail(toSend, emailTxt, emailSubject);
+        return "";
+    }
+
+    /**
+     * 구글 사용자 메일 계정 아이디/패스 정보
+     */
+    private class SMTPAuthenticator extends javax.mail.Authenticator {
+        public PasswordAuthentication getPasswordAuthentication() {
+            String username =  "amigotrip82@gmail.com"; // gmail 사용자;
+            String password = "machackki"; // 패스워드;
+            return new PasswordAuthentication(username, password);
+        }
+    }
+}
--- WEB-MAC-HACK-KI/src/main/java/amigo/com/web/HomeController.java
+++ WEB-MAC-HACK-KI/src/main/java/amigo/com/web/HomeController.java
@@ -1,8 +1,13 @@
 package amigo.com.web;
 
+import amigo.com.mail.AmigoMailSender;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.mail.MessagingException;
+
 
 /**
  * Created by Naver on 2017. 10. 16..
@@ -10,10 +15,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 @Slf4j
 public class HomeController {
+    public AmigoMailSender amigoMailSender = new AmigoMailSender();
 
     @GetMapping("/")
     public String home() {
         return "index";
     }
+
+    @GetMapping("/mail")
+    public String mail() {
+        try {
+            String[] emailList = { "wkddngus5@naver.com" };// 메일 보낼사람 리스트
+            String emailFromAddress = "amigotrip82@gmail.com";// 메일 보내는 사람
+            String emailMsgTxt = "메일 테스트 내용 "; // 내용
+            String emailSubjectTxt = "잘가는지 테스트 중~~~~~~~~~~";// 제목
+
+            // 메일보내기
+            amigoMailSender.postMail(emailList, emailSubjectTxt, emailMsgTxt, emailFromAddress);
+            System.out.println("모든 사용자에게 메일이 성공적으로 보내졌음~~");
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+        return "Success";
+    }
+
 }
 
