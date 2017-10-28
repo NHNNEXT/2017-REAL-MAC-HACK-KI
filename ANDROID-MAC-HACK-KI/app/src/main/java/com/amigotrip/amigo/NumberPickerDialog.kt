06cde6ccb64323a80400b1fda8d55887adcda9de
지민 <wlals822@naver.com>
2017-10-28 21:50:11 +0900

    refactored view
    * 화면 재구성

---
+++ ANDROID-MAC-HACK-KI/app/src/main/java/com/amigotrip/amigo/NumberPickerDialog.kt
@@ -0,0 +1,46 @@
+package com.amigotrip.amigo
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import android.widget.NumberPicker
+
+
+
+/**
+ * Created by Zimincom on 2017. 10. 28..
+ */
+class NumberPickerDialog : DialogFragment() {
+
+    private var valueChangeListener: NumberPicker.OnValueChangeListener? = null
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+
+        val numberPicker = NumberPicker(activity)
+
+        numberPicker.minValue = 20
+        numberPicker.maxValue = 60
+
+        val builder = AlertDialog.Builder(activity)
+        builder.setTitle("Choose Value")
+        builder.setMessage("Choose a number :")
+
+
+        builder.setPositiveButton("ok"  ) { _, _ ->
+            valueChangeListener?.onValueChange(numberPicker, numberPicker.value, numberPicker
+                    .value)
+        }
+
+        builder.setNegativeButton("CANCEL") { dialog, which ->
+            valueChangeListener?.onValueChange(numberPicker,
+                    numberPicker.value, numberPicker.value)
+        }
+        builder.setView(numberPicker)
+        return builder.create()
+    }
+
+    fun setValueChangeListener(valueChangeListener: NumberPicker.OnValueChangeListener) {
+        this.valueChangeListener = valueChangeListener
+    }
+}
--- ANDROID-MAC-HACK-KI/app/src/main/java/com/amigotrip/amigo/activities/NewPartyActivity.kt
+++ ANDROID-MAC-HACK-KI/app/src/main/java/com/amigotrip/amigo/activities/NewPartyActivity.kt
@@ -1,10 +1,15 @@
 package com.amigotrip.amigo.activities
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.View
+import android.widget.DatePicker
+import android.widget.NumberPicker
 import android.widget.Toast
+import com.amigotrip.amigo.NumberPickerDialog
 import com.amigotrip.amigo.R
 import com.amigotrip.amigo.datas.Party
 import com.amigotrip.amigo.remote.AmigoService
@@ -14,9 +19,18 @@ import retrofit2.Callback
 import retrofit2.Response
 import java.util.regex.Pattern
 
-class NewPartyActivity : AppCompatActivity() {
+
+class NewPartyActivity : AppCompatActivity(),
+        View.OnClickListener,
+        NumberPicker.OnValueChangeListener,
+        DatePickerDialog.OnDateSetListener {
+
 
     var gender = "male"
+
+    val amigoService: AmigoService by lazy {
+        AmigoService.getService(AmigoService::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,10 +41,6 @@ class NewPartyActivity : AppCompatActivity() {
         supportActionBar?.title = "Amigo"
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        val amigoService = AmigoService.getService(AmigoService::class.java)
-
-
-
         radio_group.setOnCheckedChangeListener { radiogrop, id ->
             run {
                 when (id) {
@@ -40,48 +50,10 @@ class NewPartyActivity : AppCompatActivity() {
             }
         }
 
+        btn_submit.setOnClickListener(this)
+        tv_select_age.setOnClickListener(this)
+        tv_choose_date.setOnClickListener(this)
 
-
-
-        btn_submit.setOnClickListener {
-
-            val name = input_name.text.toString()
-            //val age = input_age.text.toString().toInt()
-
-            val email = input_email.text.toString()
-
-            if (isEmailValid(email)) {
-                Toast.makeText(this, "apply done", Toast.LENGTH_SHORT).show()
-                startActivity(Intent(MainActivity@ this, NewPartyActivity::class.java))
-            } else {
-                input_email.error = "wrong email!"
-            }
-
-            val language = input_theme.text.toString()
-            val theme = input_attraction.text.toString()
-            val attraction = input_attraction.text.toString()
-
-            val party =
-                    Party(name, "wlals822@naver.com", 20, gender, language, "12/24", theme,
-                            attraction)
-            val call = amigoService.newParty(party)
-
-            call.enqueue(object : Callback<Party> {
-                override fun onFailure(call: Call<Party>?, t: Throwable?) {
-                    Log.d("retrofit", "onfail")
-                    Log.d("new party", t.toString())
-                }
-
-                override fun onResponse(call: Call<Party>?, response: Response<Party>?) {
-
-                    if (response!!.isSuccessful) {
-                        Log.d("retrofit", "on response success")
-                    }
-                }
-            })
-
-            startActivity(Intent(NewPartyActivity@ this, DoneActivity::class.java))
-        }
     }
 
     fun isEmailValid(email: String): Boolean {
@@ -91,6 +63,70 @@ class NewPartyActivity : AppCompatActivity() {
         val matcher = pattern.matcher(email)
 
         return matcher.matches()
+    }
+
+
+    override fun onValueChange(p0: NumberPicker?, p1: Int, p2: Int) {
+        tv_select_age.text = p1.toString()
+    }
+
+    override fun onDateSet(p0: DatePicker?, p1: Int, p2: Int, p3: Int) {
+        tv_choose_date.text =  "date:" + p1 + "/" + p2 + "/" +  p3
+    }
+
+    override fun onClick(view: View?) {
+        when(view) {
+            btn_submit -> {
+                val name = input_name.text.toString()
+                //val age = input_age.text.toString().toInt()
+
+                val email = input_email.text.toString()
+
+                if (isEmailValid(email)) {
+                    Toast.makeText(this, "apply done", Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(MainActivity@ this, NewPartyActivity::class.java))
+                } else {
+                    input_email.error = "wrong email!"
+                }
+
+                val language = input_theme.text.toString()
+                val theme = input_attraction.text.toString()
+                val attraction = input_attraction.text.toString()
+
+                val party =
+                        Party(name, "wlals822@naver.com", 20, gender, language, "12/24", theme,
+                                attraction)
+                val call = amigoService.newParty(party)
+
+                call.enqueue(object : Callback<Party> {
+                    override fun onFailure(call: Call<Party>?, t: Throwable?) {
+                        Log.d("retrofit", "onfail")
+                        Log.d("new party", t.toString())
+                    }
+
+                    override fun onResponse(call: Call<Party>?, response: Response<Party>?) {
+
+                        if (response!!.isSuccessful) {
+                            Log.d("retrofit", "on response success")
+                        }
+                    }
+                })
+                startActivity(Intent(NewPartyActivity@ this, DoneActivity::class.java))
+            }
+            tv_select_age -> {
+                val numberPickerDialog = NumberPickerDialog()
+                numberPickerDialog.setValueChangeListener(this)
+                numberPickerDialog.show(supportFragmentManager, "time picker");
+            }
+            tv_choose_date -> {
+                val datePickerDialog =
+                        DatePickerDialog(NewPartyActivity@this, this,2017, 11, 0)
+                datePickerDialog.show()
+            }
+            tv_choose_lang -> {
+
+            }
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
