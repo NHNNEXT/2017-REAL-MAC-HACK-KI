c92dcc57bb67d376241a258b1ad175c4f386d8a3
지민 <wlals822@naver.com>
2017-11-20 10:55:02 +0900

    Add chat view

--- ANDROID-MAC-HACK-KI/app/src/main/java/com/amigotrip/android/fragments/ChatsFragment.kt
+++ ANDROID-MAC-HACK-KI/app/src/main/java/com/amigotrip/android/fragments/ChatsFragment.kt
@@ -6,15 +6,20 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.amigotrip.android.adpaters.ChatRoomListAdapter
 import com.amigotrip.anroid.R
+import kotlinx.android.synthetic.main.fragment_chats.*
 
 
 class ChatsFragment : Fragment() {
-
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater!!.inflate(R.layout.fragment_chats, container, false)
     }
 
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        list_chat.adapter = ChatRoomListAdapter()
+    }
 }
