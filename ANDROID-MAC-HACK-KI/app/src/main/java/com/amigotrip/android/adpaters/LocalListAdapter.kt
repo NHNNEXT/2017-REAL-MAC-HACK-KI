0c10ead0090ac314ff09a92e428af4b6067a8ece
지민 <wlals822@naver.com>
2017-11-24 21:43:50 +0900

    Add preference screen
    + refactoer main list animation

--- ANDROID-MAC-HACK-KI/app/src/main/java/com/amigotrip/android/adpaters/LocalListAdapter.kt
+++ ANDROID-MAC-HACK-KI/app/src/main/java/com/amigotrip/android/adpaters/LocalListAdapter.kt
@@ -4,6 +4,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.amigotrip.android.ZoomOutPageTransformer
 import com.amigotrip.android.datas.Post
 import com.amigotrip.android.datas.User
 import com.amigotrip.anroid.R
@@ -43,54 +44,7 @@ class LocalListAdapter : RecyclerView.Adapter<LocalListAdapter.ViewHolder>() {
             itemView.tv_title.text = post.title
             itemView.tv_name.text = post.user.name
             itemView.pager_preview.adapter = PreviewPagerAdapter()
-            itemView.pager_preview.setPageTransformer(true) {
-
-                page, position ->
-
-                val MIN_SCALE = 0.5f
-                val MIN_ALPHA = 0.5f
-
-                var pageHeight = page.height
-                var pageWidth = page.width
-
-                if (position.toInt() < -1) {
-                    page.alpha = 0f
-                } else if (position.toInt() <= 2) {
-
-                    // 0번에서 -1 번 으로 이동시 크기 조절
-                    if (position >= 0 && position < 1) {
-                        page.scaleY = (1 - position)
-                        page.scaleX = 1 + (1 - position)
-                    } else if (position >= -1 && position < 0) {
-                        //position < 0
-                        page.scaleY = Math.min(2f, 2 + position / 2)
-                        page.scaleX = Math.min(2f, 2 + position / 2)
-                    } else if (position >= 1) {
-                        page.scaleY = 0.5f
-                    }
-//                    var scaleFactor = Math.max(MIN_SCALE, Math.abs(position))
-//                    var vertMargin = pageHeight * (1 - scaleFactor) / 2
-//                    var horzMargin = pageWidth * (1 - scaleFactor) / 2
-//
-//                    if (position > 0.1f && position < 0.5f) {
-//
-//                    }
-//
-//                    if (position > 0) {
-//                        page.translationX = horzMargin - vertMargin/2
-//                    } else {
-//                        page.translationX = -horzMargin + vertMargin / 2
-//                    }
-//
-//                    page.scaleX = scaleFactor
-//                    page.scaleY = scaleFactor
-//
-//                    page.alpha = MIN_ALPHA +
-//                            (scaleFactor - MIN_SCALE) / (1 - MIN_SCALE) * (1 - MIN_ALPHA)
-                } else {
-                    page.alpha = 0f
-                }
-            }
+            itemView.pager_preview.setPageTransformer(true, ZoomOutPageTransformer())
 
             //indicator 가 같이 움직이는 버그
             itemView.indicator.setViewPager(itemView.pager_preview)
