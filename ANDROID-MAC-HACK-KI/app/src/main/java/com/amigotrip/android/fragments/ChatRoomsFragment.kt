bec53c50201bb6525ad790b27fb898f3676b1910
지민 <wlals822@naver.com>
2017-11-26 19:14:38 +0900

    Show Chat list when logined

--- ANDROID-MAC-HACK-KI/app/src/main/java/com/amigotrip/android/fragments/ChatRoomsFragment.kt
+++ ANDROID-MAC-HACK-KI/app/src/main/java/com/amigotrip/android/fragments/ChatRoomsFragment.kt
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import com.amigotrip.android.UserInfoManager
 import com.amigotrip.android.activities.ChatRoomActivity
 import com.amigotrip.android.adpaters.ChatRoomListAdapter
@@ -30,15 +31,20 @@ class ChatRoomsFragment : Fragment(), ChatRoomListAdapter.OnChatRoomClickListene
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
+        if (UserInfoManager.isUserLogin()) {
+            list_chat.visibility = View.VISIBLE
+            tv_empty_list.visibility = View.INVISIBLE
+        }
+
+        input_search_room.setOnClickListener {
+            view -> (view as EditText).isCursorVisible = true 
+        }
         roomRef = database.getReference()
 
         val adapter = ChatRoomListAdapter()
         adapter.setOnRoomClickLisetener(this)
         list_chat.adapter = adapter
-
-        if (!UserInfoManager.isUserLogin()) {
-//            list_chat.visibility = View.INVISIBLE
-        }
 
     }
 
