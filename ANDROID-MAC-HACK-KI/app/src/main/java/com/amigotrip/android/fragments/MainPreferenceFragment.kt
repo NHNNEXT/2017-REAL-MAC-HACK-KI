96e3fb37522e335d52fb0fb5a537020f4561fd73
Zimin <wlals822@naver.com>
2017-11-29 23:09:27 +0900

    Remove unsupporting feature views (#83)
    Remove unsupporting feature view

--- ANDROID-MAC-HACK-KI/app/src/main/java/com/amigotrip/android/fragments/MainPreferenceFragment.kt
+++ ANDROID-MAC-HACK-KI/app/src/main/java/com/amigotrip/android/fragments/MainPreferenceFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.support.v7.preference.Preference
 import android.support.v7.preference.PreferenceFragmentCompat
 import android.support.v7.preference.PreferenceManager
+import android.support.v7.preference.PreferenceScreen
 import com.amigotrip.android.UserInfoManager
 import com.amigotrip.android.activities.StartActivity
 import com.amigotrip.anroid.R
@@ -12,7 +13,9 @@ import com.amigotrip.anroid.R
 /**
  * Created by Zimincom on 2017. 11. 20..
  */
-class MainPreferenceFragment : PreferenceFragmentCompat(), PreferenceManager.OnPreferenceTreeClickListener{
+class MainPreferenceFragment : PreferenceFragmentCompat(), PreferenceManager
+.OnPreferenceTreeClickListener,PreferenceFragmentCompat.OnPreferenceStartScreenCallback{
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         addPreferencesFromResource(R.xml.preference)
@@ -37,5 +40,11 @@ class MainPreferenceFragment : PreferenceFragmentCompat(), PreferenceManager.OnP
 
         startActivity(Intent(activity, StartActivity::class.java)
                 .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
+    }
+
+
+    override fun onPreferenceStartScreen(caller: PreferenceFragmentCompat?, pref: PreferenceScreen?): Boolean {
+        caller?.setPreferenceScreen(pref)
+        return true
     }
 }
