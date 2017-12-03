5a785df081778d29ba98fe1387b102130be28fa8
지민 <wlals822@naver.com>
2017-12-03 17:26:07 +0900

    Pass email to article screen

--- ANDROID-MAC-HACK-KI/app/src/main/java/com/amigotrip/android/activities/DetailActivity.kt
+++ ANDROID-MAC-HACK-KI/app/src/main/java/com/amigotrip/android/activities/DetailActivity.kt
@@ -3,11 +3,19 @@ package com.amigotrip.android.activities
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.amigotrip.anroid.R
+import timber.log.Timber
 
 class DetailActivity : AppCompatActivity() {
+
+    private lateinit var email: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+
+        email = intent.getStringExtra("email")
+        Timber.d(email)
+
     }
+
 }
