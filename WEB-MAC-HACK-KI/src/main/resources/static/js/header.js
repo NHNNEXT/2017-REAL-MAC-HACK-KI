3226aff30e7a6e7a3308cd9b3b7cebe707c010b9
bbq923 <bbq9234@naver.com>
2017-12-17 17:18:50 +0900

    sign up & in modal markup

--- WEB-MAC-HACK-KI/src/main/resources/static/js/header.js
+++ WEB-MAC-HACK-KI/src/main/resources/static/js/header.js
@@ -1,30 +1,3 @@
-window.onload = () => {
-    init();
-}
-
-function init() {
-    let dimmer = document.querySelector("#dimmer");
-    let loginBtn = document.querySelector(".login-button");
-    let signupBtn = document.querySelector(".signup-button");
-
-    loginBtn.addEventListener("click", () => {
-        dimmer.style.display = "block";
-        document.querySelector("#sign_in_modal").style.display = "block";
-    });
-
-    dimmer.addEventListener("click", () => {
-        dimmer.style.display = "none";
-        offModal();
-    })
-
-    function offModal() {
-        let modals = document.querySelectorAll(".my_modal");
-        for (modal of modals) {
-            modal.style.display = "none";
-        }
-    }
-}
-
 /**
  * Created by Woohyeon on 2017. 12. 13..
  */
