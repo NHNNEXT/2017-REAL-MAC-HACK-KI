c42ae8fe7fab2cfca935f3f3baa062fd91115c02
JangWoohyeon <wkddngus5@naver.com>
2018-01-02 10:19:31 +0900

    main page illust and about link (#131)

--- WEB-MAC-HACK-KI/src/main/resources/static/js/main.js
+++ WEB-MAC-HACK-KI/src/main/resources/static/js/main.js
@@ -3,9 +3,19 @@
  */
 class Main {
   constructor() {
+    this.arrow = document.querySelector('div.about-button');
+    this.aboutButton = document.querySelector('a.about-button');
     document.querySelector('#search-button').addEventListener('click', e => {
       this.search();
     });
+
+    this.aboutButton.addEventListener('mouseover', e => {
+      this.arrow.classList.add('active');
+    });
+
+    this.aboutButton.addEventListener('mouseout', e => {
+      this.arrow.classList.remove('active');
+    })
   }
 }
 
